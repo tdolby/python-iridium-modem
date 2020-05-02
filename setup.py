ecc884a529b15a97a76ae246601b334c5bdc764d
Trevor Dolby <github@tdolby.demon.co.uk>
2020-05-02 12:29:32 +0000

    Add README.md with correct content type

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from distutils.cmd import Command
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 tests_require = []
@@ -63,7 +63,7 @@ setup(
     version='0.1.0',
     description='Iridium phone library',
     long_description=long_description,
-
+    long_description_content_type="text/markdown",
     # The project's main homepage.
     url='https://github.com/tdolby/python-iridium-modem',
 
